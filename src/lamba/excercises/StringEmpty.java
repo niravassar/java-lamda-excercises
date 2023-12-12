654df07f78c61708c12dbbd1fd0f061f3bf96a15
niravassar-hitrust <nirav.assar@hitrustalliance.net>
2023-12-12 09:06:15 -0600

    first

---
+++ src/lamba/excercises/StringEmpty.java
@@ -0,0 +1,6 @@
+package lamba.excercises;
+
+@FunctionalInterface
+public interface StringEmpty {
+    boolean isEmpty(String s);
+}
---
+++ src/lamba/excercises/SumCalculator.java
@@ -0,0 +1,6 @@
+package lamba.excercises;
+
+@FunctionalInterface
+public interface SumCalculator {
+    int sum(int a, int b);
+}
