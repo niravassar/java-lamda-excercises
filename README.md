3be206d2ec364803243d7b8d0087a58cf1b2f8f8
niravassar-hitrust <nirav.assar@hitrustalliance.net>
2023-12-13 15:04:39 -0600

    excercises

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-https://www.hackerrank.com/challenges/java-lambda-expressions/problem
+Stream Excercises
+
+https://medium.com/@bhaskarsharan/practice-java-streams-questions-8a76cbfee1be
+
+Solution
+
+https://github.com/khmarbaise/medium-examples/blob/main/src/main/java/com/soebes/medium/stream/question/App.java
---
+++ src/stream/practice/App.java
@@ -0,0 +1,70 @@
+package stream.practice;
+
+import lamba.excercises.StringEmpty;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class App {
+
+    static List<Employee> employeeList = new ArrayList<>();
+
+    public static void main(String[] args) throws Exception {
+
+        EmployeeFactory employeeFactory = new EmployeeFactory();
+        employeeList = employeeFactory.getAllEmployee();
+
+        // TODO
+        // QnA
+        listAllDistinctProjectInNonAscendingOrder();
+        printEmployeeNameStartsWithA();
+        listEmployeesJoinedInYear2023();
+        printMinSalary();
+        listPeopleWorkingOn2Projects();
+    }
+
+    private static void listAllDistinctProjectInNonAscendingOrder() {
+        System.out.println("==== listAllDistinctProjectInNonAscendingOrder =====");
+        App.employeeList.stream()
+                .flatMap(p -> p.getProjects().stream())
+                .sorted(Comparator.comparing(Project::getName, Comparator.reverseOrder()))
+                .distinct()
+                .forEach(App::convertToString);
+    }
+
+    private static void printEmployeeNameStartsWithA() {
+        System.out.println("=== printEmployeeNameStartsWithA ======= ");
+        App.employeeList.stream()
+                .filter(employee -> employee.getFirstName().startsWith("A"))
+                .forEach(App::convertToString);
+
+    }
+
+    private static void listEmployeesJoinedInYear2023() {
+        System.out.println("=== listEmployeesJoinedInYear2023 ======= ");
+        App.employeeList.stream()
+                .filter(employee -> employee.getId().startsWith("2023"))
+                .forEach(App::convertToString);
+    }
+
+    private static void printMinSalary() {
+        System.out.println("=== printMinSalary ======= ");
+        Employee employee = App.employeeList.stream()
+                .min(Comparator.comparing(Employee::getSalary)).orElseThrow();
+        convertToString(employee);
+    }
+
+    private static void listPeopleWorkingOn2Projects() {
+        System.out.println("=== listPeopleWorkingOn2Projects ======= ");
+        List<Employee> employees = App.employeeList.stream()
+                .filter(e -> e.getProjects().size() > 2).toList();
+        employees.forEach(App::convertToString);
+    }
+
+    private static void convertToString(Project project) {
+        System.out.printf("%-12s PM: %20s Team:%s%n", project.getName(), project.getProjectManager(), project.getTeam());
+    }
+    private static void convertToString(Employee employee) {
+        System.out.printf("id: %-12s sal:%8d fn:%-8s ln:%-10s%n", employee.getId(), employee.getSalary(), employee.getFirstName(), employee.getLastName());
+    }
+}
